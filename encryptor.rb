d6b179e6788fb16dd75aca8b045a91322b18159b
Rcamach <rudycamacho@Rudys-MacBook-Pro.local>
2014-05-29 15:31:45 -0600

    Updated Encryptor

---
+++ encryptor.rb
@@ -0,0 +1,51 @@
+class Encryptor
+  def cipher
+
+
+   {'A' => 'N',
+    'B' => 'O',
+    'C' => 'P',
+    'D' => 'Q',
+    'E' => 'R',
+    'F' => 'S',
+    'G' => 'T',
+    'H' => 'U',
+    'I' => 'V',
+    'J' => 'W',
+    'K' => 'X',
+    'L' => 'Y',
+    'M' => 'Z',
+    'N' => 'A',
+    'O' => 'B',
+    'P' => 'C',
+    'Q' => 'D',
+    'R' => 'E',
+    'S' => 'F',
+    'T' => 'G',
+    'U' => 'H',
+    'V' => 'I',
+    'W' => 'J',
+    'X' => 'K',
+    'Y' => 'L',
+    'Z' => 'M'}
+  end
+
+  def encrypt (letter)
+    cipher[letter.upcase]
+  end
+
+  def encrypt_word(string)
+    results = []
+    letters = string.split('')
+    letters.each do |letter|
+      encrypt(letter)
+      results << encrypt(letter)
+      end
+    string = results.join
+  end
+end
+
+
+e = Encryptor.new
+
+puts e.encrypt_word("ehql")
